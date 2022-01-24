d016dc15d56b6d94d5524456011ef31f85ec579f
Aleksandra Wędołowska <ithuriel1010@gmail.com>
2022-01-24 13:56:30 +0100

    Ray interactor is turned on and off by menu button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class GameManager : MonoBehaviour
 {
@@ -15,11 +18,39 @@ public class GameManager : MonoBehaviour
     public delegate void TrashHandler();
     public static event TrashHandler TrashThrownAway;
 
+    public XRRayInteractor ray;
     public static int playerScore = 0;
-
+    // and other right hand buttons
+    private bool _leftTriggerDown;
+    public XRNode inputSource;
     private void Start()
     {
         eGameStatus = GameState.Playing;
+    }
+
+    private void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        ProcessInputDeviceButton(device, InputHelpers.Button.MenuButton, ref _leftTriggerDown,
+            () => // On Button Down
+            {
+                Debug.Log("Left hand trigger down");
+                // Your functionality
+                
+                if (ray.gameObject.activeSelf == false)
+                {
+                    ray.gameObject.SetActive(true);
+                }
+                else if (ray.gameObject.activeSelf == true)
+                {
+                    ray.gameObject.SetActive(false);
+                }
+
+            },
+            () => // On Button Up
+            {
+                Debug.Log("Left hand trigger up");
+            });
     }
     public static void TrashDisposed()
     {
@@ -31,6 +62,30 @@ public class GameManager : MonoBehaviour
         else
         {
             Debug.Log("Not in play mode");
+        }
+    }
+
+
+    private void ProcessInputDeviceButton(InputDevice inputDevice, InputHelpers.Button button, ref bool _wasPressedDownPreviousFrame, Action onButtonDown = null, Action onButtonUp = null, Action onButtonHeld = null)
+    {
+        if (inputDevice.IsPressed(button, out bool isPressed) && isPressed)
+        {
+            if (!_wasPressedDownPreviousFrame) // // this is button down
+            {
+                onButtonDown?.Invoke();
+            }
+
+            _wasPressedDownPreviousFrame = true;
+            onButtonHeld?.Invoke();
+        }
+        else
+        {
+            if (_wasPressedDownPreviousFrame) // this is button up
+            {
+                onButtonUp?.Invoke();
+            }
+
+            _wasPressedDownPreviousFrame = false;
         }
     }
 }
--- Assets/Scripts/RayToggle.cs
+++ Assets/Scripts/RayToggle.cs
@@ -1,18 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.XR;
 
-public class RayToggle : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+//public class RayToggle : MonoBehaviour
+//{
+//    private bool _leftTriggerDown;
+//    private bool _leftGripDown;
+//    // and other left hand buttons
+ 
+//    private bool _rightTriggerDown;
+//    private bool _rightGripDown;
+//    // and other right hand buttons
+//    public XRNode inputSource;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+
+//    private void Update()
+//    {
+//        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+//        ProcessInputDeviceButton(device, InputHelpers.Button.MenuButton, ref _leftTriggerDown,
+//            () => // On Button Down
+//            {
+//                Debug.Log("Left hand trigger down");
+//                // Your functionality
+
+//                if (gameObject.activeSelf == false)
+//                {
+//                    gameObject.SetActive(true);
+//                }
+//                else if (gameObject.activeSelf == true)
+//                {
+//                    gameObject.SetActive(false);
+//                }
+
+//            },
+//            () => // On Button Up
+//            {
+//                Debug.Log("Left hand trigger up");
+//            });
+//    }
+ 
+//private void ProcessInputDeviceButton(InputDevice inputDevice, InputHelpers.Button button, ref bool _wasPressedDownPreviousFrame, Action onButtonDown = null, Action onButtonUp = null, Action onButtonHeld = null)
+//{
+//    if (inputDevice.IsPressed(button, out bool isPressed) && isPressed)
+//    {
+//        if (!_wasPressedDownPreviousFrame) // // this is button down
+//        {
+//            onButtonDown?.Invoke();
+//        }
+ 
+//        _wasPressedDownPreviousFrame = true;
+//        onButtonHeld?.Invoke();
+//    }
+//    else
+//    {
+//        if (_wasPressedDownPreviousFrame) // this is button up
+//        {
+//            onButtonUp?.Invoke();
+//        }
+ 
+//        _wasPressedDownPreviousFrame = false;
+//    }
+//}
+//}
+
+
